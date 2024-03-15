d6c56b285b8b21dad97bf705545c99530334101e
--global <--global>
2024-03-15 15:12:36 -0300

    commit

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Fotografias(models.Model):
+    pass
+    
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    descricao = models.TextField(blank=False)
+    legenda = models.CharField(max_length=50, null=False, blank=False)
+    foto = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return f'Fotografia {self.nome}'
--- galeria/views.py
+++ galeria/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+fotografias = [
+    {"nome": "Nebula PeipipiPOPOPOPO"}
+]
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', {"cards": fotografias} )
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
