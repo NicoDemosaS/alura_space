968a0faaec27b817ff368f0dababc80dc2573bd2
--global <--global>
2024-04-02 19:36:36 -0300

    clean cadastro " "

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -73,3 +73,13 @@ class CadastroForms(forms.Form):
             }
         )
     )
+    
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get("nome_cadastro")
+        
+        if nome:
+            nome = nome.strip()
+            if ' ' in nome:
+                raise forms.ValidationError("Não é possivel inserir espaços dentro do campo")
+            else:
+                return nome 
