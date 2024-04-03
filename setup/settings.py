6ed89e3526cb01b6b7be84920e326b78ad57cd63
--global <--global>
2024-04-03 02:55:48 -0300

    reorganização de diretorios

--- setup/settings.py
+++ setup/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
-    'usuarios.apps.UsuariosConfig',
+    'apps.galeria.apps.GaleriaConfig',
+    'apps.usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
